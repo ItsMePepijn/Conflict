9c9f0a41d9fe26ab842e0ee79945fbf8dfe31325
ItsMePepijn <68758035+ItsMePepijn@users.noreply.github.com>
2023-05-16 19:55:15 +0200

    not working implementation of snowflake id system for future use

--- Conflict/Server/Hubs/ChatHub.cs
+++ Conflict/Server/Hubs/ChatHub.cs
@@ -1,12 +1,15 @@
 ﻿using Microsoft.AspNetCore.SignalR;
+using Conflict.Shared;
+using FlakeId;
 
 namespace Conflict.Server.Hubs
 {
     public class ChatHub : Hub
     {
-        public async Task SendMessage(string user, string message)
+        public async Task SendMessage(string user, string content)
         {
-            await Clients.All.SendAsync("ReceiveMessage", user, message);
+            Message Message = new(content, Id.Create());
+            await Clients.All.SendAsync("ReceiveMessage", user, Message);
         }
     }
 }
---
+++ Conflict/Shared/Message.cs
@@ -0,0 +1,23 @@
+﻿using FlakeId;
+
+namespace Conflict.Shared
+{
+    public class Message
+    {
+        public Id Id {  get; set; }
+
+        public string? Content { get; set; }
+
+        public Id ChannelId { get; set; }
+
+
+        public Message(string content, Id id)
+        {
+            Content = content;
+            //Id = id;
+            Id = Id.Create();
+
+            Console.WriteLine($"Created new message!\nContent: {Content}\nId: {Id}\n");
+        }
+    }
+}
