f07fa04df671db455639e533414dd9b33dccceb9
ItsMePepijn <68758035+ItsMePepijn@users.noreply.github.com>
2023-05-16 13:51:11 +0200

    Implemented SignalR into backend

--- Conflict/Server/Program.cs
+++ Conflict/Server/Program.cs
@@ -1,3 +1,4 @@
+using Conflict.Server.Hubs;
 using Microsoft.AspNetCore.ResponseCompression;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -6,6 +7,13 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllersWithViews();
 builder.Services.AddRazorPages();
+
+builder.Services.AddSignalR();
+builder.Services.AddResponseCompression(opts =>
+{
+    opts.MimeTypes = ResponseCompressionDefaults.MimeTypes.Concat(
+        new[] { "application/octet-stream" });
+});
 
 var app = builder.Build();
 
@@ -16,6 +24,7 @@ if (app.Environment.IsDevelopment())
 }
 else
 {
+    app.UseResponseCompression();
     app.UseExceptionHandler("/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
@@ -31,6 +40,9 @@ app.UseRouting();
 
 app.MapRazorPages();
 app.MapControllers();
+
+app.MapHub<ChatHub>("/chathub");
+
 app.MapFallbackToFile("index.html");
 
 app.Run();
