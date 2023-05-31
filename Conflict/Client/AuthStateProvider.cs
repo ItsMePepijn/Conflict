736b70033b887c278e256be55e16d8acc03dd3f4
ItsMePepijn <68758035+ItsMePepijn@users.noreply.github.com>
2023-05-31 11:37:22 +0200

    authentication

---
+++ Conflict/Client/AuthStateProvider.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.AspNetCore.Components.Authorization;
+using System.Net.Http.Headers;
+using System.Security.Claims;
+using System.Text.Json;
+using Blazored.LocalStorage;
+
+
+namespace Conflict.Client
+{
+	public class AuthStateProvider : AuthenticationStateProvider
+	{
+		private readonly ILocalStorageService _localStorage;
+		private readonly HttpClient _http;
+
+		public AuthStateProvider(ILocalStorageService localStorage, HttpClient http)
+		{
+			_localStorage = localStorage;
+			_http = http;
+		}
+
+		public override async Task<AuthenticationState> GetAuthenticationStateAsync()
+		{
+			string token = await _localStorage.GetItemAsStringAsync("token");
+
+            ClaimsIdentity identity = new();
+			_http.DefaultRequestHeaders.Authorization = null;
+
+			if (!string.IsNullOrEmpty(token))
+			{
+				identity = new ClaimsIdentity(ParseClaimsFromJwt(token), "jwt");
+				_http.DefaultRequestHeaders.Authorization =
+					new AuthenticationHeaderValue(token.Replace("\"", ""));
+			}
+
+            ClaimsPrincipal user = new(identity);
+            AuthenticationState state = new(user);
+
+			NotifyAuthenticationStateChanged(Task.FromResult(state));
+
+			return state;
+		}
+
+		public static IEnumerable<Claim> ParseClaimsFromJwt(string jwt)
+		{
+            string payload = jwt.Split('.')[1];
+			byte[] jsonBytes = ParseBase64WithoutPadding(payload);
+			var keyValuePairs = JsonSerializer.Deserialize<Dictionary<string, object>>(jsonBytes)!;
+
+			return keyValuePairs.Select(kvp => new Claim(kvp.Key, kvp.Value.ToString()!));
+		}
+
+		private static byte[] ParseBase64WithoutPadding(string base64)
+		{
+			switch (base64.Length % 4)
+			{
+				case 2: base64 += "=="; break;
+				case 3: base64 += "="; break;
+			}
+			return Convert.FromBase64String(base64);
+		}
+	}
+}
--- Conflict/Client/Program.cs
+++ Conflict/Client/Program.cs
@@ -1,11 +1,16 @@
 using Conflict.Client;
+using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Blazored.LocalStorage;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddScoped<AuthenticationStateProvider, AuthStateProvider>();
+builder.Services.AddAuthorizationCore();
+builder.Services.AddBlazoredLocalStorage();
 
 await builder.Build().RunAsync();
