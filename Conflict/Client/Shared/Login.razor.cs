b065d4d16b8a991dbc0f2d4183ed0db45ed15d0e
ItsMePepijn <68758035+ItsMePepijn@users.noreply.github.com>
2023-05-31 11:38:48 +0200

    login

---
+++ Conflict/Client/Shared/Login.razor.cs
@@ -0,0 +1,19 @@
+﻿using Conflict.Shared.Dto;
+using System.Net.Http.Json;
+
+namespace Conflict.Client.Shared
+{
+	partial class Login
+	{
+        UserDto user = new();
+
+        async Task HandleLogin()
+        {
+            var result = await Http.PostAsJsonAsync("api/auth/login", user);
+            var token = await result.Content.ReadAsStringAsync();
+            Console.WriteLine(token);
+            await LocalStorage.SetItemAsync("token", token);
+            await AuthStateProvider.GetAuthenticationStateAsync();
+        }
+    }
+}
