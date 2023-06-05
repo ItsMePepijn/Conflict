f88c2de6ce2ab6416a0c25836fa53f1f5b104061
ItsMePepijn <68758035+ItsMePepijn@users.noreply.github.com>
2023-06-05 11:25:04 +0200

    removed hub function for sending messages

--- Conflict/Server/Hubs/ChatHub.cs
+++ Conflict/Server/Hubs/ChatHub.cs
@@ -1,26 +1,25 @@
 ﻿using Microsoft.AspNetCore.SignalR;
 using FlakeId;
-using Conflict.Shared.Models;
 
 namespace Conflict.Server.Hubs
 {
     public class ChatHub : Hub
     {
-        public async Task SendMessage(string username, string content)
-        {
-            User User = new()
-            {
-                Name = "Username",
-                Id = Id.Create()
-            };
-            Message Message = new()
-            {
-                Content = content,
-                Id = Id.Create(),
-                Author = User,
-            };
+        //public async Task SendMessage(string username, string content)
+        //{
+        //    User User = new()
+        //    {
+        //        Name = "Username",
+        //        Id = Id.Create()
+        //    };
+        //    Message Message = new()
+        //    {
+        //        Content = content,
+        //        Id = Id.Create(),
+        //        Author = User,
+        //    };
 
-            await Clients.All.SendAsync("ReceiveMessage", Message);
-        }
+        //    await Clients.All.SendAsync("ReceiveMessage", Message);
+        //}
     }
 }
