9a91e9ce7c6d98ec6a26de577a54bd58ddbef5af
ItsMePepijn <68758035+ItsMePepijn@users.noreply.github.com>
2023-06-08 13:48:25 +0200

    provider

---
+++ Conflict/Client/Providers/AuthStateProvider.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Components.Authorization;
+using System.Net.Http.Headers;
+using System.Security.Claims;
+using Conflict.Shared;
+using Blazored.LocalStorage;
+
+
+namespace Conflict.Client.Providers
+{
+    public class AuthStateProvider : AuthenticationStateProvider
+    {
+        private readonly ILocalStorageService _localStorage;
+        private readonly HttpClient _http;
+
+        public AuthStateProvider(ILocalStorageService localStorage, HttpClient http)
+        {
+            _localStorage = localStorage;
+            _http = http;
+        }
+
+        public override async Task<AuthenticationState> GetAuthenticationStateAsync()
+        {
+            string token = await _localStorage.GetItemAsStringAsync("token");
+
+            ClaimsIdentity identity = new();
+            _http.DefaultRequestHeaders.Authorization = null;
+
+            if (!string.IsNullOrEmpty(token))
+            {
+                identity = new ClaimsIdentity(JwtProvider.ParseClaimsFromJwt(token), "jwt");
+                _http.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token.Replace("\"", ""));
+            }
+
+            ClaimsPrincipal user = new(identity);
+            AuthenticationState state = new(user);
+
+            NotifyAuthenticationStateChanged(Task.FromResult(state));
+
+            return state;
+        }
+    }
+}
