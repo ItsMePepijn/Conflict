91f3434ff99fb2c0111f730db89a253918c6b0ff
ItsMePepijn <68758035+ItsMePepijn@users.noreply.github.com>
2023-06-08 13:49:29 +0200

    channel state

--- Conflict/Client/Program.cs
+++ Conflict/Client/Program.cs
@@ -11,6 +11,7 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddScoped<AuthenticationStateProvider, AuthStateProvider>();
+builder.Services.AddSingleton<ChannelStateProvider>();
 builder.Services.AddSingleton<HubConnectionProvider>();
 builder.Services.AddAuthorizationCore();
 builder.Services.AddBlazoredLocalStorage();
