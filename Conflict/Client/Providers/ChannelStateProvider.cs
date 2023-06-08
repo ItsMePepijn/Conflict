ca104016ec81b9ac7a655b42fd8f5bf3e914ef69
ItsMePepijn <68758035+ItsMePepijn@users.noreply.github.com>
2023-06-08 13:43:43 +0200

    moved to providers

---
+++ Conflict/Client/Providers/ChannelStateProvider.cs
@@ -0,0 +1,17 @@
+﻿namespace Conflict.Client.Providers
+{
+    public class ChannelStateProvider
+    {
+        public long? Id { get; private set; } = null;
+
+        public event Action? OnChange;
+
+        public void SetChannel(long? channelId)
+        {
+            Id = channelId;
+            NotifyStateChanged();
+        }
+
+        private void NotifyStateChanged() => OnChange?.Invoke();
+    }
+}
