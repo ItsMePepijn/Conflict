c0336d8beb353472f6541af8ba662cf125cca9f7
ItsMePepijn <68758035+ItsMePepijn@users.noreply.github.com>
2023-06-08 13:49:00 +0200

    message list

---
+++ Conflict/Client/Shared/App/ChannelList.razor.cs
@@ -0,0 +1,40 @@
+﻿using Conflict.Shared.Models;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.SignalR.Client;
+using System.Net.Http.Json;
+
+namespace Conflict.Client.Shared.App
+{
+    partial class ChannelList
+    {
+        public List<Channel>? Channels { get; set; } = null;
+
+        protected override async Task OnInitializedAsync()
+        {
+            var result = await Http.GetFromJsonAsync<Channel[]>("api/channels");
+            if(result is not null)
+            {
+                Channels = new();
+                Channels.AddRange(result);
+            }
+
+			ConnectionProvider.HubConnection.On<Channel>("ChannelAdded", (channel) =>
+            {
+                if(Channels is not null)
+                {
+                    Channels.Add(channel);
+                    StateHasChanged();
+                }
+            });
+        }
+
+        public void HandleChannelClick(long channelId)
+        {
+            if (ChannelState.Id == channelId)
+                ChannelState.SetChannel(null);
+            else
+                ChannelState.SetChannel(channelId);
+
+		}
+    }
+}
