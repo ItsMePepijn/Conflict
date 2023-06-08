6ceac471c59fc57a337d23a8459a879f4b46b1b2
ItsMePepijn <68758035+ItsMePepijn@users.noreply.github.com>
2023-06-08 13:48:17 +0200

    hubconnection

---
+++ Conflict/Client/Providers/HubConnectionProvider.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.SignalR.Client;
+
+namespace Conflict.Client.Providers
+{
+	public class HubConnectionProvider
+	{
+		public HubConnection HubConnection { get; set; }
+		public HubConnectionProvider(NavigationManager Navigation)
+		{
+			HubConnection = new HubConnectionBuilder()
+				.WithUrl(Navigation.ToAbsoluteUri("/chathub"))
+				.WithAutomaticReconnect(new RetryPolicy())
+				.Build();
+
+		}
+
+	}
+}
