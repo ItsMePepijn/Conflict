6a59b6b79cd7f1d92aeb66ad2bd4ef39a7676fe9
ItsMePepijn <68758035+ItsMePepijn@users.noreply.github.com>
2023-06-11 12:44:39 +0200

    not needed constructor

--- Conflict/Shared/Models/Channel.cs
+++ Conflict/Shared/Models/Channel.cs
@@ -3,11 +3,6 @@ namespace Conflict.Shared.Models
 {
     public class Channel
     {
-        public Channel(long Id, string Name)
-        {
-            this.Id = Id;
-            this.Name = Name;
-        }
 
         public long Id { get; set; } = 0;
         public string Name { get; set; } = string.Empty;
