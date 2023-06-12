74a1d1cef710c495c4b2a0ac5b7e243e685b0f97
ItsMePepijn <68758035+ItsMePepijn@users.noreply.github.com>
2023-06-12 08:52:45 +0200

    removed comment

--- Conflict/Server/Hubs/ChatHub.cs
+++ Conflict/Server/Hubs/ChatHub.cs
@@ -5,21 +5,5 @@ namespace Conflict.Server.Hubs
 {
     public class ChatHub : Hub
     {
-        //public async Task SendMessage(string username, string content)
-        //{
-        //    User User = new()
-        //    {
-        //        Name = "Username",
-        //        Id = Id.Create()
-        //    };
-        //    Message Message = new()
-        //    {
-        //        Content = content,
-        //        Id = Id.Create(),
-        //        Author = User,
-        //    };
-
-        //    await Clients.All.SendAsync("ReceiveMessage", Message);
-        //}
     }
 }
